122f9d315f7da7e7a109144f17a254fe9cc28172
Andrew Chen <andrewchen.2563@gmail.com>
2025-01-15 12:09:32 -0500

    movement updates
    trying to implement something similar to this video. very useful tutorial on parrying and hitboxes https://www.youtube.com/watch?v=4mmarlzESE8

--- Scripts/movement.gd
+++ Scripts/movement.gd
@@ -1,33 +1,40 @@
 extends CharacterBody2D
 
-@export var speed := 10000.0
+var screen_size
 
-var parryLock = false
+func _ready():
+	screen_size = get_viewport_rect().size
+
+@export var speed : float = 150
+var can_move : bool = true:
+	set(value):
+		can_move = value
+		if value == false:
+			speed = 0
+		else:
+			speed = 150
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(_delta):
-	
-	#PECK MOVEMENT
-	var horizontal_direction = Input.get_axis("move_left", "move_right")
-	var vertical_direction = Input.get_axis("move_up", "move_down")
-	
-	velocity.y = _delta * speed * vertical_direction
-	velocity.x = _delta * speed * horizontal_direction
-
+func _physics_process(delta):
+	velocity = Input.get_vector("move_left","move_right","move_up","move_down") * speed
 	move_and_slide()
 	
-	#PECK IDLE ANIMATION
-	if velocity.x == 0 && velocity.y == 0:
-		$Peck.play("IDLE")
-	
-	#PECK MOVE ANIMATION
-	if horizontal_direction != 0 || vertical_direction != 0:
-		if horizontal_direction <0:
-				$Peck.flip_h = true
-		if horizontal_direction >0:
-				$Peck.flip_h = false
-				
+	if velocity.length() > 0:
 		$Peck.play("walk")
+	else:
+		$Peck.play("IDLE")
+		
+	
+	position += velocity * delta
+	
+	#Locks Peck to Screen
+	position = position.clamp(Vector2.ZERO, screen_size)
+		
+	if velocity.x < 0:
+		$Peck.flip_h = true
+	elif velocity.x > 0:
+		$Peck.flip_h = false
+		
 		
 	#PECK PARRY
 	#The parry needs to lock the player in place for the duration. 
@@ -37,3 +44,8 @@ func _physics_process(_delta):
 
 	if Input.is_action_pressed("parry"):
 			$Peck.play("parryAttempt")
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
