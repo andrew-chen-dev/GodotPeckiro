2dc75a97df3c22818c78e45fb1f321d42a9d570f
Andrew Chen <andrewchen.2563@gmail.com>
2025-01-22 10:59:25 -0500

    Added attack animation on x key
    Code is a bit of a mess but works thanks to @BangPeter. I cleaned up some of the earlier code that's unneeded now. A lot of the visual bugs came from the AnimationPlayer Reset track not resetting postion to 0,0 and instead to 20,0.
    
    The attack animation is peck_attack in animationplayer
    
    The attacking and parrying boolean is awkwardly implemented in movement.gd.
    
    Also note that a lot of the AnimationPlayer animations don't move the CollisionShape2D Peck hitbox yet and that should be implemented.
    
    The chicken audio continues to haunt me. please edit that down to the first 2 seconds so you dont get the full 30 second clip. please.
    
    The eventual goal is is one demo level of you vs the farmer. You have 3 HP, he has 3 HP. The farmer walks towards you and uses his stab attack when close.

--- Scripts/movement.gd
+++ Scripts/movement.gd
@@ -7,12 +7,12 @@ signal goodParry
 
 func _ready():
 	screen_size = get_viewport_rect().size
-	animation.play("peck_idle")
 
 @export var speed : float = 150
 @export var parrying = false
 @export var attacking = false
-@export var is_facing_right = true
+@export var isFacingRight = true
+
 
 @export var can_move : bool = true:
 	set(value):
@@ -25,85 +25,57 @@ func _ready():
 func _process(delta):
 	if Input.is_action_just_pressed("parry"):
 		parry()
+	if Input.is_action_just_pressed("attack"):
+		attack()
 
-	
+	velocity = Input.get_vector("move_left","move_right","move_up","move_down") * speed
+	move_and_slide()
+	if !parrying && !attacking && can_move:
+		if velocity.length() > 0:
+			animation.play("peck_walk")
+		else:
+			animation.play("peck_idle")
+	if (velocity.x < 0 && isFacingRight) || (!isFacingRight && velocity.x > 0):
+		print("condition met")
+		isFacingRight = !isFacingRight
+		$".".scale.x = -1
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+
+
 func _physics_process(delta):
 	if time > 0:
 		time -= delta
-	velocity = Input.get_vector("move_left","move_right","move_up","move_down") * speed
-	move_and_slide()
-	
-	if can_move:
-		if velocity.length() > 0:
-			animation.play("peck_walk")
-			print("walking")
-		else:
-			animation.play("peck_idle")
-			print("idle")
-		if velocity.x < 0:
-			$Peck.flip_h = true
-			
-		if velocity.x > 0:
-			$Peck.flip_h = false
-			
 
-		
-
-		
 	position += velocity * delta
 	
 	#Locks Peck to Screen
 	position = position.clamp(Vector2.ZERO, screen_size)
-		
-		
 
-	
-		
-	#PECK PARRY
-	#The parry needs to lock the player in place for the duration. 
-	#Additionally, there should be a short active invincible parry window and a longer inactive window where you're stuck in animation.
-	#Missing a parry should be punishable.
+
+#PECK PARRY
+#The parry needs to lock the player in place for the duration. 
+#Additionally, there should be a short active invincible parry window and a longer inactive window where you're stuck in animation.
+#Missing a parry should be punishable.
 	
 func parry():
-	parrying = true
-	print("parrying")
-	animation.play("peck_parry")
+	if can_move:
+		parrying = true
+		print("parrying")
+		animation.play("peck_parry")
 
+func attack():
+	if can_move:
+		attacking = true
+		print("attacking")
+		animation.play("peck_attack")
 
-
-
-func update_animation():
-	if !parrying:
-		if velocity.length() > 0:
-			animation.play("peck_walk")
-		else:
-			animation.play("peck_idle")
-		if velocity.x < 0:
-			$Peck.flip_h = true
-
-		elif velocity.x > 0:
-			$Peck.flip_h = false
-
-
-		
-	
-	
-#func _input(event):
-	#
-#
-#func _on_animation_finished(anim_name: StringName) -> void:
-	#can_move = false
 
 
 func start(pos):
 	position = pos
 	show()
 	$CollisionShape2D.disabled = false
-
-
-
 
 func _on_reflecting_parry_success() -> void:
 	goodParry.emit()
